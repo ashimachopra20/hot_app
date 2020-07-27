f778a52db9d4873740cd2bcd3c3791ae0f66f9fa
ashimachopra20 <64543480+ashimachopra20@users.noreply.github.com>
2020-07-27 17:36:57 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import "ui/home.dart";
+
+
+main() {
+  runApp(hotapp());
+}
+class hotapp extends StatelessWidget{
+  
+  build(BuildContext c1) {
+    // TODO: implement build
+   return MyApp();
+    
+  }
+}
